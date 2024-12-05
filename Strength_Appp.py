23690a2cdaa617551ec724a70bf6fd031f2a3f22
anyooooooooo <23e1812c@shinshu-u.ac.jp>
2024-12-05 17:06:43 +0900

    4

--- Strength_Appp.py
+++ Strength_Appp.py
@@ -5,10 +5,10 @@ def calculate_one_rep_max(weight, reps):
         return weight
     return round(weight * (1 + 0.0333 * reps), 2)
 
-st.title("Max重量は？")
-st.write("最大重量計算アプリ")
+st.title("BIG3最大重量計算アプリ")
+
 st.write("あなたが上げられるMax重量を提示します！！")
-st.write("種目ごとに重量と回数を入力して、最大重量を計算します。")
+st.write("BIG3の種目ごとに重量と回数を入力して、最大重量を計算します。")
 exercises = ["ベンチプレス", "デッドリフト", "スクワット"]
 results = {}
 
